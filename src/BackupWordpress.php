a5e83487784a52cd37a6b429b70eeea18f0740df
Ian Service <ian@service.to>
2016-01-25 14:57:04 -0500

    various fixes for command line calls and picking out host name

--- src/BackupWordpress.php
+++ src/BackupWordpress.php
@@ -8,6 +8,8 @@ class BackupWordpress {
 	private $tar = "/bin/tar";
 	private $bzip2 = "/usr/bin/bzip2";
 
+	private $properties = [];
+
 	function backup($filesystem) {
 		if ($dh = opendir($this->configdir)) {
 			while (false !== ($entry = readdir($dh))) {
@@ -20,50 +22,77 @@ class BackupWordpress {
 	}
 
 	function checkForBackup($httpdconfigfile, $filesystem) {
+		$this->properties = [];
+		$documentroot = "";
 		if (file_exists($httpdconfigfile)) {
 			$lines = file($httpdconfigfile);
 			foreach ($lines as $line) {
+				if (substr(trim($line), 0, strlen("ServerName")) == "ServerName") {
+					$properties["servername"] = trim(substr(trim($line), strlen("ServerName") + 1), " \t\n\r\0\x0B\"");
+				}
 				if (substr(trim($line), 0, strlen("DocumentRoot")) == "DocumentRoot") {
 					$documentroot = trim(substr(trim($line), strlen("DocumentRoot") + 1), " \t\n\r\0\x0B\"");
-					print("Looking in " . $documentroot . "\n");
-					if ($this->checkForWordpress($documentroot)) {
-						$this->backupWordpress($documentroot, $filesystem);
-					}
 				}
+			}
+			print("Looking in " . $documentroot . "\n");
+			if ($this->checkForWordpress($documentroot)) {
+				$this->backupWordpress($documentroot, $filesystem);
 			}
 		}
 	}
 
 	function checkForWordpress($documentroot) {
-		if (file_exists($documentroot . "/wp_config.php")) {
-			print("Wordpress config file found in " . $documentroot . "/wp_config.php\n")
-			$lines = file($documentroot . "/wp_config.php");
+		if (file_exists($documentroot . "/wp-config.php")) {
+			print("Wordpress config file found in " . $documentroot . "/wp-config.php\n");
+			$lines = file($documentroot . "/wp-config.php");
 			foreach ($lines as $line) {
 				if (substr($line, 0, strlen("define")) == "define") {
-					print(trim($line) . "\n");
-					eval(trim($line));
+					$definition = substr(trim($line), strpos(trim($line), "(") + 1, strlen(trim($line)) - strpos(trim($line), "(") - strrpos(trim($line), ")") + 2);
+					list($name, $value) = preg_split("/,/", $definition);
+					$name = trim($name, " \t\n\r\0\x0B\"'");
+					$value = trim($value, " \t\n\r\0\x0B\"'");
+					$this->properties[$name] = $value;
 				}
 			}
+			return true;
 		}
 		return false;
 	}
 
+
 	function backupWordpress($documentroot, $filesystem) {
-		if (DB_NAME && DB_USER && DB_PASSWORD && DB_HOST) {
-			$tempfile = $this->tempdir . DB_NAME . "." . date("Y-m-d-h-i-s") . ".sql";
+		if ($this->properties["DB_NAME"] && $this->properties["DB_USER"] && $this->properties["DB_PASSWORD"] && $this->properties["DB_HOST"]) {
+			$tempfile = $this->tempdir . $this->properties["DB_NAME"] . "." . date("Y-m-d-h-i-s") . ".sql";
 			print("Backing up database to " . $tempfile . "\n");
 
-			system($this->mysqldump . " -h " . DB_HOST . " -u " . DB_USER . " -p" . DB_PASSWORD . " " . DB_NAME . " > " . $tempfile);
+			system($this->mysqldump . " -h " . $this->properties["DB_HOST"] . " -u " . $this->properties["DB_USER"] . " -p" . $this->properties["DB_PASSWORD"] . " --lock-tables=false " . $this->properties["DB_NAME"] . " > " . $tempfile);
 
 			// compress file.
+			print("Compressing...\n");
 			system($this->bzip2 . " " . $tempfile);
 
+			print("Pushing to Flysystem\n");
 			$stream = fopen($tempfile . ".bz2", "r+");
-			$filesystem->writeStream("backups/" . DB_NAME . "." . strfttime("%G-%m-%d") . ".bz2", $stream);
+			$filesystem->writeStream("backups/" . $this->properties["DB_NAME"] . "." . date("Y-m-d-h-i-s") . ".sql.bz2", $stream);
 
 			if (is_resource($stream)) {
 				fclose($stream);
 			}
+
+			// back up the wordpress content
+			$tempfile = $this->tempdir . $properties["servername"] . "." . date("Y-m-d-h-i-s") . ".tar.bz2";
+			print("Backing up files to " . $tempfile . "\n");
+
+			system($this->tar . " -cjf " . $tempfile . " -C " . $documentroot . " .");
+
+			print("Pushing to Flysystem\n");
+			$stream = fopen($tempfile, "r+");
+			$filesystem->writeStream("backups/" . $properties["servername"] . "." . date("Y-m-d-h-i-s") . ".tar" , $stream);
+
+			if (is_resource($stream)) {
+				fclose($stream);
+			}
+
 		}
 		return false;
 	}
